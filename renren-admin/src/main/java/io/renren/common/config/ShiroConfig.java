c4029a68ed2e1714275eff1ab623c76259f9a1a7
llc <787254039@qq.com>
2019-01-11 16:52:11 +0800

    更新

--- renren-admin/src/main/java/io/renren/common/config/ShiroConfig.java
+++ renren-admin/src/main/java/io/renren/common/config/ShiroConfig.java
@@ -88,6 +88,7 @@ public class ShiroConfig {
         filterMap.put("/sys/login", "anon");
         filterMap.put("/favicon.ico", "anon");
         filterMap.put("/captcha.jpg", "anon");
+        filterMap.put("/upload/**", "anon");
         filterMap.put("/**", "authc");
         shiroFilter.setFilterChainDefinitionMap(filterMap);
 
